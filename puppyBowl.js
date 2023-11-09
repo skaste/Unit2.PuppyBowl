9e57c5f5e902e2973f7e3f7733b3e410a67a11b2
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-09 10:28:06 -0600

    created the try catch around the fetch data to log if errors exist during fetch

--- puppyBowl.js
+++ puppyBowl.js
@@ -1,2 +1,18 @@
-const apiBaseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`
+const apiBaseURL = `https://fsa-puppy-bowl.herokuapp.com/api/2310-FSA-ET-WEB-FT-SF/`;
 console.log(apiBaseURL);
+
+const state = {
+  allPlayers: [],
+};
+
+// Take all players and show them on the page
+const getAllPlayers = async () => {
+  try {
+    const data = await fetch(apiBaseURL + "players");
+    const jsonResponse = await response.json();
+    state.allPlayers = jsonResponse.data;
+    console.log(jsonResponse);
+  } catch (error) {
+    console.log(error)
+  }
+};
